36a728ee10dd925972d986255649c27def18163a
fredtruix <fredricktrust@gmail.com>
2023-01-08 23:34:23 +0100

    add:model & install user

--- Safemeet/user/models.py
+++ Safemeet/user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your views here.
+
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    image = models.ImageField(blank=True)
+
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
